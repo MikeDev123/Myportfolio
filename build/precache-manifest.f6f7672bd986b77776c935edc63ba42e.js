debf8436fc9c4a300d876478ffe5f410318aebdf
Mike <64442647+MikeDev123@users.noreply.github.com>
2020-06-11 09:10:23 -0700

    Add files via upload
    adding robofriends project

---
+++ build/precache-manifest.f6f7672bd986b77776c935edc63ba42e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1481eb567fecb9accd390884ee90ff61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "521283a0a4b54b6d0a2b",
+    "url": "/static/css/main.bf037629.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521283a0a4b54b6d0a2b",
+    "url": "/static/js/main.a1747c2b.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "92c3810f48104a0fa26cbef3ad302807",
+    "url": "/static/media/electrical.92c3810f.woff"
+  }
+]);
